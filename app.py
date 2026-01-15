e1c11c01b871f5521980f7f4b0681c5a76789cd1
rmrusev24a <rmrusev24a@gmail.com>
2026-01-15 10:15:30 +0200

    Create a Streamlit app for favorite colors and sports
    This Streamlit app allows users to select their favorite colors and sports, storing the results in session state and displaying them as bar charts.

---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pandas as pd
+
+st.title("📊 Любими неща – класна анкета")
+
+if "colors" not in st.session_state:
+    st.session_state.colors = {
+        "Червен": 0,
+        "Син": 0,
+        "Зелен": 0,
+        "Жълт": 0
+    }
+
+if "sports" not in st.session_state:
+    st.session_state.sports = {
+        "Футбол": 0,
+        "Баскетбол": 0,
+        "Волейбол": 0,
+        "Плуване": 0
+    }
+
+st.subheader("Избери любими неща")
+
+color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
+sport = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+
+if st.button("Запази избора"):
+    st.session_state.colors[color] += 1
+    st.session_state.sports[sport] += 1
+    st.success("Изборът е записан!")
+
+st.divider()
+
+st.subheader("☑️ Резултати")
+
+st.write("Любими цветове")
+colors_df = pd.DataFrame.from_dict(
+    st.session_state.colors, orient="index", columns=["Брой"]
+)
+st.bar_chart(colors_df)
+
+st.write("Любими спортове")
+sports_df = pd.DataFrame.from_dict(
+    st.session_state.sports, orient="index", columns=["Брой"]
+)
+st.bar_chart(sports_df)
